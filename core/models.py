ad0736483458543d9299821600ba33c990784556
laubohlen <laurensbohlen@hotmail.com>
2024-04-22 15:48:49 +0200

    added models Animal and IbexImage

--- core/models.py
+++ core/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from filer.models.abstract import BaseImage as FilerBaseImage
 
 
 class User(AbstractUser):
     pass
+
+
+class Animal(models.Model):
+    id_code = models.CharField(max_length=8, null=True, blank=True)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    marked = models.BooleanField(default=False)
+    capture_date = models.DateField(null=True, blank=True)
+    cohort = models.CharField(max_length=4, null=True, blank=True)
+    sex = models.CharField(max_length=1, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.id_code
+
+
+class IbexImage(FilerBaseImage):
+    animal = models.ForeignKey(Animal, on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta(FilerBaseImage.Meta):
+        # You must define a meta with en explicit app_label
+        app_label = "core"
+        default_manager_name = "objects"
