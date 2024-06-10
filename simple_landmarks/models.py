758a77ec5fe79cc30c9cc3fb88d38f7bd88b11a5
laubohlen <laurensbohlen@hotmail.com>
2024-06-10 16:18:18 +0200

    separate landmark and landmark_item models

--- simple_landmarks/models.py
+++ simple_landmarks/models.py
@@ -5,6 +5,13 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 class Landmark(models.Model):
     label = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.label
+
+
+class LandmarkItem(models.Model):
+    landmark = models.ForeignKey(Landmark, on_delete=models.CASCADE)
     x_coordinate = models.PositiveBigIntegerField()
     y_coordinate = models.PositiveIntegerField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
