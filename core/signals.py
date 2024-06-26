ba8fb8ddff559b3349f2cbe10d122424258d75c7
laubohlen <laurensbohlen@hotmail.com>
2024-06-26 16:08:07 +0200

    initialise landmark items upon image upload

--- core/signals.py
+++ core/signals.py
@@ -6,8 +6,10 @@ from django.db.models.signals import post_save
 from django.utils.encoding import force_str
 from django.utils.timezone import now
 from django.utils.text import get_valid_filename as get_valid_filename_django
+from django.contrib.contenttypes.models import ContentType
 
 from .models import IbexImage
+from simple_landmarks.models import LandmarkItem, Landmark
 
 
 @receiver(post_save, sender=IbexImage)
@@ -33,5 +35,20 @@ def rename_uploaded_image(sender, instance, created, **kwargs):
         )
         image.name = get_valid_filename_django(new_filename)
         image.save()
+    else:
+        pass
+
+
+@receiver(post_save, sender=IbexImage)
+def initialise_landmark_items(sender, instance, created, **kwargs):
+    image = instance
+    if created:
+        # initialise landmark-items for each landmark for the new image
+        content_type = ContentType.objects.get_for_model(IbexImage)
+        landmarks = Landmark.objects.all()
+        for lm in landmarks:
+            LandmarkItem.objects.create(
+                content_type=content_type, object_id=image.id, landmark=lm
+            )
     else:
         pass
