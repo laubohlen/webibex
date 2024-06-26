2ed346433ffc1107cb8b5962ac1ca8d393de4209
laubohlen <laurensbohlen@hotmail.com>
2024-06-26 16:09:16 +0200

    create views for adding landmarks

--- core/views.py
+++ core/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.db.models.aggregates import Count
 from django.contrib.contenttypes.models import ContentType
 
 from core.models import IbexImage, Animal
-from simple_landmarks.models import LandmarkItem
+from simple_landmarks.models import LandmarkItem, Landmark
+
+
+# snipet from https://github.com/krasch/simple_landmarks
+# coordinates are sent as slightly weird URL parameters (e.g. 0.png?214,243)
+# parse them, will crash server if they are coming in unexpected format
+def parse_coordinates(request):
+    keys = list(request.GET.keys())
+    assert len(keys) == 1
+    coordinates = keys[0]
+
+    assert len(coordinates.split(",")) == 2
+    x, y = coordinates.split(",")
+    x = int(x)
+    y = int(y)
+    return x, y
 
 
 def welcome_view(request):
@@ -54,10 +69,76 @@ def unobserved_animal_view(request):
     )
 
 
+def to_landmark_images_view(request):
+    # get all images that dont feature any landmarks AND are not featured to any animal
+    images_to_landmark = IbexImage.objects.filter(animal_id__isnull=True)
+    return render(
+        request,
+        "core/to_landmark.html",
+        {"images": images_to_landmark},
+    )
+
+
+def landmark_horn_view(request, oid):
+    image = IbexImage.objects.filter(id=oid).first()
+    return render(
+        request,
+        "simple_landmarks/horn_landmark.html",
+        {"image": image},
+    )
+
+
+def landmark_eye_view(request, oid):
+    x_horn, y_horn = parse_coordinates(request)
+
+    # save horn-landmark for that image
+    landmark_id = Landmark.objects.get(label="horn_tip").id
+    content_type = ContentType.objects.get_for_model(IbexImage)
+    landmark_item = get_object_or_404(
+        LandmarkItem,
+        content_type=content_type,
+        object_id=oid,
+        landmark_id=landmark_id,
+    )
+    landmark_item.x_coordinate = x_horn
+    landmark_item.y_coordinate = y_horn
+    landmark_item.save()
+
+    # render eye_landmark page
+    image = IbexImage.objects.filter(id=oid).first()
+    return render(
+        request,
+        "simple_landmarks/eye_landmark.html",
+        {"image": image},
+    )
+
+
+def finished_landmark_view(request, oid):
+    x_eye, y_eye = parse_coordinates(request)
+
+    # save eye-landmark for that image
+    landmark_id = Landmark.objects.get(label="eye_corner").id
+    content_type = ContentType.objects.get_for_model(IbexImage)
+    landmark_item = get_object_or_404(
+        LandmarkItem,
+        content_type=content_type,
+        object_id=oid,
+        landmark_id=landmark_id,
+    )
+    landmark_item.x_coordinate = x_eye
+    landmark_item.y_coordinate = y_eye
+    landmark_item.save()
+
+    # return view of the landmarks on the image
+    # together with confirmation button
+    pass
+
+
 def test_view(request):
     content_type = ContentType.objects.get_for_model(IbexImage)
 
     queryset = LandmarkItem.objects.select_related("landmark").filter(
-        content_type=content_type
+        content_type=content_type,
+        object_id=198,
     )
     return render(request, "core/test.html", {"queryset": queryset})
