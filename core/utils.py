a6724250456580a48bf00ff063859360b2d14b21
Laurens Bohlen <laurensbohlen@hotmail.com>
2025-02-27 14:05:20 +0100

    added functions: 1. percent_coordinate to calculate relative position of coordinates to the image and display them dynamically with any size of images. 2. multi_task_url to generate the correct template and context for each of the tasks that can be performed with multiple images in a loop. 3. process_horn_chip to have chip cropping, embedding and chip file handling out of the already crowded views.py file. 4. get_task_request_origin to read url of where a post request is coming from and mapping it to a django-url-pattern. Not in use atm. 5. id_color_mapping to map tailwind colors to different id's for color coding the results page.

--- core/utils.py
+++ core/utils.py
@@ -2,16 +2,24 @@ import os
 import re
 import cv2
 import math
+import shutil
 import base64
 import requests
 import datetime
 import numpy as np
 
+from django.urls import resolve
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django.core.files.base import ContentFile
 
-from core.models import Animal, Embedding
+from core.models import Animal, Embedding, IbexChip, Region
 
+from io import BytesIO
+from PIL import Image
 from environ import Env
+from pathlib import Path
+from urllib.parse import urlparse
 
 from . import b2_utils
 
@@ -59,6 +67,12 @@ def parse_coordinates(request):
 def scale_coordinate(x: int, y: int, dst_image_width: int, src_image_width: int):
     scale = dst_image_width / src_image_width
     return round(x * scale), round(y * scale)
+
+
+def percentage_coordinate(x: int, y: int, src_image_width: int, src_image_height: int):
+    left_percentage = (x / src_image_width) * 100
+    top_percentage = (y / src_image_height) * 100
+    return left_percentage, top_percentage
 
 
 # mirror coordinate along the x-axis when right horn is flipped to be normalised as a left horn
@@ -342,3 +356,223 @@ def embed_new_chip(ibex_chip):
     # Save the embedding to the database
     Embedding.objects.create(ibex_chip=ibex_chip, embedding=output)
     print("Embedding created and saved.")
+
+
+# return img
+def multi_task_url(tool, image=None, user=None):
+    if tool == "view":
+        print("Viewing")
+        return "core/multi_view.html", None
+    elif tool == "locate":
+        print("Locating images")
+        image_location = image.location
+        location_id = image_location.id
+        # check if GPS is available, else return None
+        if None in [image_location.latitude, image_location.longitude]:
+            image_location = None
+        region_qs = Region.objects.filter(owner=user)
+        template = "core/multi_location_create.html"
+        task_specific_context = {
+            "image_location": image_location,
+            "location_id": location_id,
+            "regions": region_qs,
+        }
+        return template, task_specific_context
+    elif tool == "landmark":
+        print("Landmarking images")
+        template = "simple_landmarks/multi_landmarking.html"
+        task_specific_context = {"display_width": settings.LANDMARK_IMAGE_WIDTH}
+        return template, task_specific_context
+    elif tool == "delete":
+        print("Deleting images")
+    else:
+        print("No valid tool selected.")
+
+
+def process_horn_chip(image, x_horn, y_horn, x_eye, y_eye):
+    print("image -", image)
+    chip_name = get_chip_filename(image.file.name, settings.CHIP_FILETYPE)
+    print("chip name -", chip_name)
+
+    # Determine media storage environment
+    if settings.ENVIRONMENT == "production" or settings.POSTGRES_LOCALLY == True:
+        is_local = False
+    else:
+        is_local = True
+    print("is_local -", is_local)
+
+    if is_local:
+        image_path = os.path.join(settings.MEDIA_ROOT, image.file.name)
+        chip_url = os.path.join(os.path.split(image.url)[0], chip_name)
+        chip_path = Path(os.path.join(os.path.split(image_path)[0], chip_name))
+
+        # if a chip exists already, delete it before continuing
+        if chip_path.is_file():
+            # also update database
+            ibex_chip = get_object_or_404(IbexChip, ibex_image_id=image.id)
+            ibex_chip.delete()
+            # chip_path.unlink()
+            print(
+                "IbexChip already existed on local storage, deleted successfully before continueing."
+            )
+
+        # create new chip from original image and try to preserve all metadata
+        shutil.copy2(image_path, chip_path)
+
+        # load image
+        img = load_image(chip_path)
+
+    else:
+        # if a chip exists already, delete it before continuing
+        try:
+            ibex_chip = IbexChip.objects.get(ibex_image_id=image.id)
+            # If the object is found, continue with your logic here
+            print(f"IbexChip found in database with ibex_image_id: {image.id}")
+            print("Deleting previous ibex chip media file on backblaze..")
+            # chip_public_id = ibex_chip.file.name
+            image_bucket_path = os.path.join(settings.AWS_LOCATION, image.file.name)
+            chip_bucket_path = os.path.dirname(image_bucket_path)
+            chip_bucket_path = os.path.join(chip_bucket_path, chip_name)
+            # Check if the file exists in the B2 bucket
+            file_exists = b2_utils.check_file_exists(chip_bucket_path)
+
+            # If the file exists, proceed with deletion
+            if file_exists:
+                # Delete the associated IbexChip object from the database
+                ibex_chip = get_object_or_404(IbexChip, ibex_image_id=image.id)
+                ibex_chip.delete()
+
+                # # Delete the file from Backblaze B2 bucket
+                # b2_utils.delete_files([chip_bucket_path])
+                print(
+                    f"File {chip_bucket_path} deleted from B2 bucket and IbexChip deleted from the database."
+                )
+            else:
+                print(
+                    "IbexChip not deleted because the file was not found in the B2 bucket."
+                )
+
+        except IbexChip.DoesNotExist:
+            # Handle the case where the object does not exist
+            print("IbexChip does not exist already, continueing normally..")
+            pass
+
+        # Download the image from cloud
+        bucket_file_path = os.path.join(settings.AWS_LOCATION, image.file.name)
+        img_object = b2_utils.download_file(bucket_file_path=bucket_file_path)
+        # Convert the response content to a NumPy array
+        img_array = np.frombuffer(img_object, np.uint8)
+
+        # Decode the image from the NumPy array
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+        if img is None:
+            raise ValueError("Failed to load image from cloud.")
+
+        print("Downloaded image from cloud.")
+
+    # setup landmarks
+    eyehorn_src = [
+        [x_eye, y_eye],
+        [x_horn, y_horn],
+    ]
+
+    # check animal side
+    # flip image if it is right taged
+    if image.side == "R":
+        img = cv2.flip(img, 1)  # along x-axis = around y-axis
+        # flip x-coordinates
+        eyehorn_src[0][0] = mirror_coordinate(eyehorn_src[0][0], image.width)
+        eyehorn_src[1][0] = mirror_coordinate(eyehorn_src[1][0], image.width)
+
+    # calculate coordniates where horn and eye should be in the output image
+    width_dst = settings.CHIP_WIDTH
+    height_dst = settings.CHIP_HEIGHT
+    eye_dst = (np.round(width_dst * 0.98), np.round(height_dst * 0.95))
+    tip_dst = (np.round(width_dst * 0.98), np.round(height_dst * 0.05))
+    eyehorn_dst = [eye_dst, tip_dst]
+
+    # affine transform image
+    tform = similarityTransform(eyehorn_src, eyehorn_dst)
+    # note, height and width are exchanged here because we want a
+    # horizontal image first
+    shape_dst = (width_dst, height_dst)
+    img_transformed = cv2.warpAffine(img, tform, shape_dst)
+
+    # save
+    if is_local:
+        cv2.imwrite(chip_path, cv2.cvtColor(img_transformed, cv2.COLOR_RGB2BGR))
+        print("path", chip_path)
+        chip_file = os.path.join(os.path.split(str(image.file.name))[0], chip_name)
+        print("file", chip_file)
+        IbexChip.objects.create(file=chip_file, ibex_image_id=image.id)
+        print("Chip saved locally using open-cv, database updated.")
+        ibex_chip = get_object_or_404(IbexChip, ibex_image_id=image.id)
+        print("Loaded ibex chip object")
+    else:
+        # Convert the image to the correct format for backblaze
+        buffer = BytesIO()
+        img_pil = Image.fromarray(cv2.cvtColor(img_transformed, cv2.COLOR_RGB2BGR))
+        img_pil.save(buffer, format="png")
+        buffer.seek(0)
+
+        # Use Django's FileField to handle the upload
+        chip_content = ContentFile(buffer.getvalue())
+
+        # Create the IbexChip instance
+        ibex_chip = IbexChip(ibex_image_id=image.id)
+        ibex_chip.file.save(chip_name, chip_content)
+        print("Chip saved on Backblaze using Django's FileField.")
+        chip_url = ibex_chip.file.url
+
+    # Call the custom method to process and embed the chip
+    embed_new_chip(ibex_chip)
+    return
+
+
+# catch url where user is coming from to redirect there after task
+def get_task_request_origin(request):
+    """
+    This function catches where the post request came from in order to redirect
+    the user to that origin after the task is complete.
+    Tasks, such as locating an image, or landmarking can be requested from
+    different pages (unidentified-images, image-update) but they are handled
+    by the same view function.
+    """
+    referer = request.META.get("HTTP_REFERER")
+    if referer:
+        parsed_url = urlparse(referer)
+        # parsed_url.path will be something like "/unidentified/"
+        try:
+            match = resolve(parsed_url.path)
+            task_request_url_name = match.url_name  # e.g., "unidentified-images"
+            print("Matched URL pattern:", task_request_url_name)
+        except Exception as e:
+            task_request_url_name = None
+            print("No matching URL pattern:", e)
+    return task_request_url_name
+
+
+def id_color_mapping(gallery_and_distances):
+    """Map colors to the different ID's when showing the 5 best matching horn chips."""
+    # Define your five color classes (adjust these as needed)
+    # need to be safelisted for tailwind at 'note/tailwind.config.js'
+    color_classes = [
+        "bg-blue-400",
+        "bg-purple-400",
+        "bg-orange-400",
+        "bg-slate-400",
+        "bg-emerald-400",
+    ]
+
+    # Create a dictionary mapping animal IDs to color classes.
+    # We'll iterate over gallery_and_distances (or a list of chips) and assign each new animal ID the next color.
+    id_to_color = {}
+    for chip, distance in gallery_and_distances:
+        animal_id = chip.ibex_image.animal.id
+        if animal_id not in id_to_color:
+            # Use modulo in case there are more than 5, though you mentioned max 5.
+            id_to_color[animal_id] = color_classes[
+                len(id_to_color) % len(color_classes)
+            ]
+    return id_to_color
