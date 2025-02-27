3c7825b2a62d8edcd4aa83fea8eb519b2246ee54
Laurens Bohlen <laurensbohlen@hotmail.com>
2025-02-27 14:08:03 +0100

    safelisted color classes that are defined in python and therefore not scanned with the tailwind script that collects all used classes in html-files.

--- node/tailwind.config.js
+++ node/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["../templates/*.html", "../templates/**/*.html", "../static/css/tailwind.css"],
+    // these colors are defined in a python utils function and are not scanned for tailwind classes
+    safelist: ["bg-emerald-400", "bg-blue-400", "bg-purple-400", "bg-orange-400", "bg-slate-400"],
     theme: {
         extend: {
             fontFamily: {
