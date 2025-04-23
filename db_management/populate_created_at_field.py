3e09fc8c8a8defa6d4a329f8780b6eaf64043f5d
Laurens Bohlen <laurensbohlen@hotmail.com>
2025-04-23 17:05:11 +0200

    get created date of images already in the database from the name that was created on upload; original exif seems to no longer be available after upload

--- db_management/populate_created_at_field.py
+++ db_management/populate_created_at_field.py
@@ -1,29 +1,13 @@
 import os
-import sys
 import datetime
-
-# Add project root to Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-import django
-
-# Setup Django environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webibex.settings') 
-django.setup()
-
 from core.models import IbexImage
 
 images = IbexImage.objects.all()
 
 for image in images:
-    dt = image.exif.get("DateTime", None)
-    if isinstance(dt, str):
-        dt_object = datetime.datetime.strptime(dt, "%Y:%m:%d %H:%M:%S")
-        if isinstance(dt_object, datetime.datetime):
-            image.created_at = dt_object
-        else:
-            image.created_at = datetime.datetime.now()
-    else:
-        image.created_at = datetime.datetime.now()
+    filename, file_extenstion = os.path.splitext(str(image))
+    parts = filename.split("_")[-4:]
+    dt_string = "_".join(parts)
+    dt_object = datetime.datetime.strptime(dt_string, "%y_%m_%d_%H%M%S")
+    image.created_at = dt_object
     image.save(update_fields=["created_at"])
-    print(f"Updated image {image.id}")
--- db_management/test.py
+++ db_management/test.py
@@ -1,16 +1,13 @@
+import os
 import datetime
-
 from core.models import IbexImage
 
 images = IbexImage.objects.all()
 
 for image in images:
-    dt = image.exif.get("DateTime", None)
-    if isinstance(dt, str):
-        dt_object = datetime.datetime.strptime(dt, "%Y:%m:%d %H:%M:%S")
-        if isinstance(dt_object, datetime.datetime):
-            print(dt_object)
-        else:
-            print("no datetime exif")
-    else:
-        print("no exif")
+    filename, file_extenstion = os.path.splitext(str(image))
+    parts = filename.split("_")[-4:]
+    dt_string = "_".join(parts)
+    dt_object = datetime.datetime.strptime(dt_string, "%y_%m_%d_%H%M%S")
+    image.created_at = dt_object
+    image.save(update_fields=["created_at"])
